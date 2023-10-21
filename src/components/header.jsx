8ac8bf4b87229de8938d5eff78fce8e4579bdd70
5422065mm <145961749+5422065mm@users.noreply.github.com>
2023-10-21 18:42:17 +0900

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <h1>Birdwatching</h1>
-            <img src="dove.png" alt="a simple dove logo"></img>
+            <img src="https://github.com/5422065mm/asset/blob/main/public/dove.png?raw=true" alt="a simple dove logo"></img>
             < Nav />
         </header>
     );
